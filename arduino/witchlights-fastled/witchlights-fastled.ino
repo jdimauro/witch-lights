dddcf5aad51a2d7dbeb55cf971f6432dfbdd40b7
Josh DiMauro <jdimauro@gmail.com>
2018-05-04 16:47:10 -0400

    Welp. I done broke it. It compiles and uploads but doesn't spawn the test sprite.

--- arduino/witchlights-fastled/witchlights-fastled.ino
+++ arduino/witchlights-fastled/witchlights-fastled.ino
@@ -236,7 +236,13 @@ class SpriteVector {
 
 // I want to declare a BlinkingEyesSprite and declare which animation fragments it uses, what its "loop" animation is, what the sprite animation is, its starting interval and its target interval when it reaches a destination with an UpdateTravel move. Also its travel fadeinterval and animation fadeinterval. 
 // Use ifdef to call animation char and CRGB structs into memory at compile time
-  
+
+
+// TODO: Animation Experiments!
+
+// Animation experiment classes start here.
+
+// First working animation test class. 
 /*
 class AnimationTestSprite : public Sprite {
   private:
@@ -372,6 +378,147 @@ class AnimationTestSprite : public Sprite {
 };
 */
 
+// Loop test class
+
+// Travel, pause, play loop 2-5 times, move on
+
+
+class LoopTestSprite : public Sprite {
+  private:
+    int updateInterval;
+    int currentPixel;
+    bool isScanning;
+    int scanningFrame;
+    int lastInflection;
+    int nextInflection;
+    int scanCount;
+    int scanCountTotal;
+
+    // pattern is one black pixel plus remaining pixels in order of increasing brightness with brightest pixel doubled.
+    CRGB pattern[NUM_COLORS_PER_SET + 1];
+    int patternLength = NUM_COLORS_PER_SET + 1;
+
+    void SetNextInflection() {
+        lastInflection = nextInflection;
+        nextInflection += random(SCANNER_MIN_STOP_DISTANCE, SCANNER_MAX_STOP_DISTANCE + 1);
+    }
+
+    int GetNewScanCountTotal() {
+        return random(SCANNER_MIN_SCANS, SCANNER_MAX_SCANS + 1);
+    }
+
+  public:
+    LoopTestSprite() : Sprite() {
+        // Initial state.
+        this->currentPixel = -8;  // The first pixel of the pattern is black.
+        this->scanningFrame = 0;
+        this->isScanning = false;
+        this->lastInflection = 0;
+        this->nextInflection = 0;
+        SetNextInflection();
+        this->scanCount = 0;
+        this->scanCountTotal = GetNewScanCountTotal();
+        this->updateInterval = SPRITE_STARTING_DELAY_INTERVAL_IN_MS;
+
+        // Choose a random color palette from the palettes available.
+        int colorPalette = random(0, NUM_COLORSETS);
+
+        // Set the colors in the pattern.
+        this->pattern[0] = colorSets[colorPalette][0];
+        this->pattern[1] = colorSets[colorPalette][1];
+        this->pattern[2] = colorSets[colorPalette][2];
+        this->pattern[3] = colorSets[colorPalette][3];
+        this->pattern[4] = colorSets[colorPalette][4];
+        this->pattern[5] = colorSets[colorPalette][5];
+        this->pattern[6] = colorSets[colorPalette][6];
+        this->pattern[7] = colorSets[colorPalette][7];
+        this->pattern[8] = colorSets[colorPalette][8];
+        this->pattern[9] = colorSets[colorPalette][8];
+
+        this->patternLength = 10;
+
+        for (int i = 0; i < ANIMATION_FRAME_WIDTH * ANIMATION_FRAMES; i++) {
+            af_l_pulsar_a[i] = afc_l_pulsar_a[i] > ' ' ? colorSets[colorPalette][afc_l_pulsar_a[i] - '0'] : CRGB::Black;
+        }
+    }
+
+    ~LoopTestSprite() {
+    }
+
+    boolean UpdateNow() {
+      if (millis() - lastUpdateTime >= ACCELERATION_DELAY_OBVIOUSNESS_FACTOR * updateInterval) {
+        lastUpdateTime = millis();
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    bool Update() {
+        if (! this->UpdateNow()) {
+            return false;
+        }
+
+        // Going from scanning to travel mode.
+        if (isScanning && scanCount == scanCountTotal) {
+            isScanning = false;
+            currentPixel += 8;
+            SetNextInflection();
+            this->scanCount = 0;
+            this->scanCountTotal = GetNewScanCountTotal();
+            this->updateInterval = SPRITE_STARTING_DELAY_INTERVAL_IN_MS;
+            leds[currentPixel - 6] = CRGB::Black;
+            leds[currentPixel - 8] = CRGB::Black;
+            leds[currentPixel - 9] = CRGB::Black;  // I hate this. One-off to get rid of the straggler when coming out of scan mode.
+            leds[currentPixel - 10] = CRGB::Black;
+        }
+
+        if (! isScanning) {
+            // Traveling and continuing to travel.
+            stripcpy(leds, pattern, currentPixel, patternLength, patternLength);
+            ++currentPixel;
+
+            if (currentPixel >= nextInflection - (SCANNER_DELAY_INTERVAL_IN_MS - 1)) {
+                updateInterval += 1;
+            } else {
+                updateInterval -= 1;
+            }
+
+            if (updateInterval < 1) {
+                updateInterval = 1;
+            } else if (updateInterval > SPRITE_STARTING_DELAY_INTERVAL_IN_MS) {
+                updateInterval = SPRITE_STARTING_DELAY_INTERVAL_IN_MS;
+            }
+
+            // Transition from travel mode to scanning.
+            if (currentPixel >= nextInflection) {
+                // Safety. Since I don't trust my math, once we enter scanning mode, ALWAYS go back to the constant speed for scanning
+                // regardless of what the math said.
+                updateInterval = SCANNER_DELAY_INTERVAL_IN_MS;
+                isScanning = true;
+                scanningFrame = 0;
+                currentPixel -= 8;
+            }
+
+            if (currentPixel > NUM_LEDS) {
+               this->MarkDone();
+            }
+        } else {
+            stripcpy(leds, af_l_pulsar_a + ANIMATION_FRAME_WIDTH * scanningFrame, currentPixel, afc_l_pulsar_a_ANIMATION_FRAME_WIDTH, afc_l_pulsar_a_ANIMATION_FRAME_WIDTH);
+            if (++scanningFrame == afc_l_pulsar_a_ANIMATION_FRAMES) {
+                scanningFrame = 0;
+                ++scanCount;
+                SetNextInflection();
+            }
+        }
+
+        return true;
+    }
+};
+
+
+
+// Test intro and outro fragments together until I like them, then split into two halves and adjust code to play one, a loop, and then the other. 
 class FragmentTestSprite : public Sprite {
   private:
     int updateInterval;
@@ -510,7 +657,7 @@ class FragmentTestSprite : public Sprite {
     }
 };
 
-
+// Animation sprite from last year
 class W8V1ScannerDebrisV1Sprite : public Sprite {
   private:
     int updateInterval;
@@ -1035,22 +1182,11 @@ void loop() {
         return;
     }
 
-    // (A) JOSH: Remove this when you have the switches working to your heart's content.
-/*    if (random(0, 1000) == 0) {
-        Sprite *s = new W8V1ScannerDebrisV1Sprite();
-
-        bool added = spriteManager->Add(s);
-
-        if (! added) {
-            delete s;
-        }
-    } */
-    // End (A).
-
     if (sensor1->IsActuated()) {
         // Sprite *s1 = new W8V1ScannerDebrisV1Sprite();
         // Sprite *s1 = new AnimationTestSprite();
-      Sprite *s1 = new FragmentTestSprite();
+        // Sprite *s1 = new FragmentTestSprite();
+        Sprite *s1 = new LoopTestSprite();
 
         if (! spriteManager->Add(s1)) {
             delete s1;
