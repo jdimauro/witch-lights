bba81c8214f07ff235f01c5963d28f170070f911
Jazz Rabbit <126007435+whichrabbit@users.noreply.github.com>
2023-06-20 09:15:09 -0400

    created wifiauth-example.cpp for public repo

---
+++ arduino/witchlights-fastled/wifiauth-example.cpp
@@ -0,0 +1,6 @@
+#include "wifiauth.h"
+
+// Enter the ssid and password for your wifi and rename this file to 'wifiauth.cpp'
+
+const char* ssid = "...........";
+const char* password = "...........";
