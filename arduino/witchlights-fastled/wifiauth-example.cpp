c5dafa6a61ca948da07967682700c239b0c4880a
Jazz Rabbit <126007435+whichrabbit@users.noreply.github.com>
2023-06-20 09:47:05 -0400

    oh ok fine comment out the fake wifi lines I guess

--- arduino/witchlights-fastled/wifiauth-example.cpp
+++ arduino/witchlights-fastled/wifiauth-example.cpp
@@ -1,6 +1,6 @@
 #include "wifiauth.h"
 
-// Enter the ssid and password for your wifi and rename this file to 'wifiauth.cpp'
+// Enter the ssid and password for your wifi, rename this file to 'wifiauth.cpp', and uncomment the following lines:
 
-const char* ssid = "...........";
-const char* password = "...........";
+// const char* ssid = "...........";
+// const char* password = "...........";
